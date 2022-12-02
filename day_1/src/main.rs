c01592a13bc2a7c51f80853f67507f34f0954aa3
Ziga Pregelj <pregelj.ziga@gmail.com>
2022-12-02 23:49:05 +0100

    Solved day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,38 +1,59 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::error::Error;
 
-
+#[derive(Debug)]
 struct Elf {
-    pub id: u16,
-    pub calories: u32
+    pub id: usize,
+    pub calories_sum: u32,
+    pub calories: Vec<u32>
 }
 
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
-
-fn main() {
-    // File hosts must exist in current path before this produces output
-    if let Ok(lines) = read_lines("./calories.txt") {
-        // Consumes the iterator, returns an (Optional) String
-        for line in lines {
-            if let Ok(ip) = line {
-                //println!("{}", ip);
-                if ip.is_empty() {
-                    println!("IS EMPTY")
-                }
-                else {
-                    let calories: u32 = ip.trim().parse().unwrap();
-                    Elf::default()
-                    println!("{}", calories)
-                }
+fn prepare_calories(all_calories: io::Lines<io::BufReader<File>>) -> Result<Vec<Vec<u32>>, Box<dyn Error>> {
+    let mut prepared: Vec<Vec<u32>> = Vec::new();
+    prepared.push(Vec::new());
+    for line in all_calories {
+        if let Ok(ip) = line {
+            if ip.is_empty() {
+                prepared.push(Vec::new());
+            }
+            else {
+                let calories: u32 = ip.trim().parse().unwrap();
+                prepared.last_mut().unwrap().push(calories);
             }
         }
+    }
+    Ok(prepared)
+}
+
+
+fn create_elfs(all_calories: Vec<Vec<u32>>) -> Result<Vec<Elf>, Box<dyn Error>> {
+    let mut elfs: Vec<Elf> = Vec::new();
+    for elf_calories in &all_calories {
+        let elf_index: usize = all_calories.iter().position(|x| x == elf_calories).unwrap();
+        let calories_sum: u32 = elf_calories.iter().sum();
+        elfs.push(Elf{id: elf_index, calories_sum: calories_sum, calories: elf_calories.to_vec() })
+    }
+    Ok(elfs)
+}
+
+
+fn main() {
+    if let Ok(lines) = read_lines("./calories.txt") {
+        let prepared_calories: Vec<Vec<u32>> = prepare_calories(lines).unwrap();
+        let mut elfs: Vec<Elf> = create_elfs(prepared_calories).unwrap();
+        elfs.sort_by(|a, b| b.calories_sum.cmp(&a.calories_sum));
+        let top_three_sum: u32 = elfs[0..=2].iter().map(|s| s.calories_sum).sum();
+
+        println!("Top calories elf: {:?}", elfs[0]);
+        println!("Top three elves calories sum: {:?}", top_three_sum);
     }
 }
 
