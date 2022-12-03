1640337c546f51c6bb59fc2ba3f8ef39c89cc61b
Ziga Pregelj <pregelj.ziga@gmail.com>
2022-12-03 15:47:28 +0100

    day2 WIP

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,3 +1,32 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::error::Error;
+
+
+struct PlayedHand {
+    score: u8
+}
+
+enum PlayResult {
+    Win,
+    Lose,
+    Tie
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn remove_whitespace(x : String) -> String{
+    x.replace(" ", "")
+  }
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(lines) = read_lines("./strategy.txt") {
+        for line in lines {
+            println!("{:?}", remove_whitespace(line.unwrap()));
+        }
+    }
 }
