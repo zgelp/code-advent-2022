ee9e3e4522cb2a9bca5444dfce62aadf11f0cd89
zgelp <pregelj.ziga@gmail.com>
2022-12-19 17:02:59 +0100

    Day 5 first part

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -2,13 +2,71 @@ use std::fs::File;
 use std::path::Path;
 use std::io::Read;
 use csv::ReaderBuilder;
-use std::error::Error;
 use std::collections::HashMap;
+use itertools::Itertools;
+use csv::Reader;
 
 
-// zamenji presledke z , ez csv gang rise up
+fn prepare_data(mut reader: Reader<&[u8]>) -> HashMap<usize, Vec<String>> {
+    let mut csv_data: HashMap<usize, Vec<String>> = HashMap::with_capacity(10);
+    // Parse csv and create hashmap
+    for result in reader.records() {
+        let line = result.unwrap();
+        for i in 0..9 {
+            let one_char = line[i].to_owned();
+            if csv_data.contains_key(&i) {
+                let v = csv_data.get_mut(&i).unwrap();
+                v.push(one_char);
+            } else {
+                csv_data.insert(i, vec![one_char]); 
+            }
+        }
+    }
 
-fn main() -> Result<(), Box<dyn Error>>{
+    // Sort vector values in hashmap and remove empty strings
+    let mut prepared_data: HashMap<usize, Vec<String>> = HashMap::new();
+    for (i, vec) in csv_data.iter() {
+        let reversed_vec: Vec<String> = vec.iter().rev().filter_map(|s| {
+            if s.is_empty() {
+                None
+            } else {
+                Some(s.to_string())
+            }
+        }).collect();
+        // Increment Hashmap id to get indexes as in example table
+        prepared_data.insert(*i + 1, reversed_vec);
+    }
+    prepared_data
+}
+
+fn prepare_instructions(instructions: Vec<&str>) -> Vec<(usize,usize,usize)>{
+    instructions.into_iter()
+        .map(|s| s.split(' ').collect::<Vec<&str>>())
+        .filter(|words| words[1].parse::<usize>().is_ok() && words[3].parse::<usize>().is_ok() && words[5].parse::<usize>().is_ok())
+        .map(|words| (words[1].parse::<usize>().unwrap(), words[3].parse::<usize>().unwrap(), words[5].parse::<usize>().unwrap())).collect()
+}
+
+fn arrange_supply(instructions: Vec<(usize, usize, usize)>, mut supply: HashMap<usize, Vec<String>>)  {
+
+    for instruction in instructions {
+        // Take first vector from hashmap (based on instruction id)
+        let mut first_vec = supply.get_mut(&instruction.1).unwrap();
+        // Take items from that vector and reverse
+        let mut vec_elements: Vec<String> = first_vec.drain(first_vec.len() - instruction.0..).collect();
+        vec_elements.reverse();
+
+        // Update second vector based on ID
+        let mut second_vec = supply.get_mut(&instruction.2).unwrap();
+        second_vec.extend(vec_elements);
+    }
+
+    println!("Solution First Part: ");
+    for (k, v) in supply.iter().sorted_by_key(|x| x.0) {
+        println!("{:?} => {:?}", k, v);
+    }
+}
+
+fn main() {
     let path = Path::new("supply.txt");
 
     let mut file = match File::open(path) {
@@ -24,49 +82,25 @@ fn main() -> Result<(), Box<dyn Error>>{
 
     let lines: Vec<&str> = contents.split('\n').collect();
     let data: Vec<&str> = lines[0..8].to_vec();
+    let instructions: Vec<&str> = lines[10..].to_vec();
     let mut csv_data: Vec<String> = Vec::new();
 
-    // LMAO 😂😂😂 💀 💀 💀 
+    // LMAO 💀 💀 💀
     for line in data {
-        let replace1 = line.replace("            ",",,,");
-        let replace2 = replace1.replace("         ",",,,");
+        let replace1 = line.replace("            ", ",,,");
+        let replace2 = replace1.replace("         ", ",,,");
         let replace3 = replace2.replace("     ", ",,");
-        let replace4 = replace3.replace("    ",",");
+        let replace4 = replace3.replace("    ", ",");
         let replace5 = replace4.replace(" ", ",");
         csv_data.push(replace5);
     }
 
     let string_from_vec = csv_data.join("\n");
-    let mut rdr = ReaderBuilder::new()
+    let rdr = ReaderBuilder::new()
         .has_headers(false)
         .from_reader(string_from_vec.as_bytes());
 
-    let mut csv_data: HashMap<usize, Vec<String>> = HashMap::with_capacity(10);
-    
-    for result in rdr.records() {
-        let record = result.unwrap();
-        for i in 0..9 {
-            let one_char = record[i].to_owned();
-            if csv_data.contains_key(&i) {
-                let v = csv_data.get_mut(&i).unwrap();
-                v.push(one_char);
-            } else {
-                csv_data.insert(i, vec![one_char]); }
-        }
-    }
-
-    let mut prepared_data: HashMap<usize, Vec<String>> = HashMap::new();
-    for (i, vec) in csv_data.iter() {
-        let reversed_vec: Vec<String> = vec.iter().rev().filter_map(|s| {
-            if s.is_empty() {
-                None
-            } else {
-                Some(s.to_string())
-            }
-        }).collect();
-        prepared_data.insert(*i, reversed_vec);
-    }
-
-    println!("{:?}", prepared_data);
-    Ok(())
+    let mut supply_data: HashMap<usize, Vec<String>> = prepare_data(rdr);
+    let supply_instructions = prepare_instructions(instructions);
+    arrange_supply(supply_instructions, supply_data);
 }
